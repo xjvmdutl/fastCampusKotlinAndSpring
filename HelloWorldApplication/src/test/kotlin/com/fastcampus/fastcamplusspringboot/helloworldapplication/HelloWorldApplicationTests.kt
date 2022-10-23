63fa6b54dcbbeffff3dfbafdc93084d097a4cb90
junhokim <junhokim@gmarket.com>
2022-10-23 15:32:59 +0900

    스프링 부트 스타트 - 스프링 부트 이해하기

---
+++ HelloWorldApplication/src/test/kotlin/com/fastcampus/fastcamplusspringboot/helloworldapplication/HelloWorldApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fastcampus.fastcamplusspringboot.helloworldapplication
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
