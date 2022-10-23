a799aff1c589f2934353453742fd047804c385b1
junhokim <junhokim@gmarket.com>
2022-10-23 21:55:01 +0900

    JAVA 프로젝트에 코틀린 도입하기 - Getter/Setter - JvmStatic - 예외

---
+++ fastcampus-kotlin-java-master/kotlin-java/src/com/fastcampus/kotlinjava/exception/KotlinThrow.kt
@@ -0,0 +1,21 @@
+package com.fastcampus.kotlinjava.exception
+
+import java.io.IOException
+
+class KotlinThrow {
+
+    @Throws(IOException::class) //강제로 예외 전파를 하고 싶을 때 사용
+    fun throwIOException() {
+        throw IOException("체크드 익셉션인 IOException 발생")
+    }
+}
+
+fun main() {
+    //예외를 전파하거나, try-catch를 사용하지 않아도 된다(강제하지 않는다)
+    val javaThrow = JavaThrow()
+    javaThrow.throwIOException()
+
+    val kotlinThrow = KotlinThrow()
+    kotlinThrow.throwIOException()
+
+}
---
+++ fastcampus-kotlin-java-master/kotlin-java/src/com/fastcampus/kotlinjava/gettersetter/JavaGetterSetterExample.java
@@ -0,0 +1,32 @@
+package com.fastcampus.kotlinjava.gettersetter;
+
+import java.time.LocalDate;
+
+public class JavaGetterSetterExample {
+
+  public static void main(String[] args) {
+    Student student = new Student();
+    //setter
+    //student.setName("스티브로저스");
+    //student.setBirthDate(LocalDate.of(1918, 7, 4));
+
+    //getter
+    //System.out.println(student.getName());
+    //System.out.println(student.getBirthDate());
+
+    //student.setAge(10);//컴파일 오류 //val 키워드로 선언한 프로퍼티는 setter가 없다.
+    System.out.println(student.getAge());
+
+
+    //student.setGrade(""); //var 키워드지만 private로 선언했기 때문에 외부에서 호출 x
+    student.changeGrade("A"); //setter가 아닌 볊도의 메서드를 만들어서 값을 변경하는 방식으로 사용해야한다
+    System.out.println(student.getGrade());
+
+
+    //JVM 필드 사용
+    student.name = "스티브로저스";
+    System.out.println(student.name);
+    //JVMfield를 사용하면 getter, setter를 통해서 접근하는 것이 아닌 프로퍼티로 접근해야한다
+  }
+
+}
